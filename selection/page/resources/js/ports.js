ab91b21f41098397043ca4cb2a58c0992e893941
Nigel Belderbos <nigel.belderbos@outlook.com>
2023-12-05 17:58:13 +0100

    update to ports

--- selection/page/resources/js/ports.js
+++ selection/page/resources/js/ports.js
@@ -1,33 +1,20 @@
 async function get_ports() {
     for (let i = 5000; i < 5010; i++) {
-        try {
-            let res = new XMLHttpRequest();
-            // document.location.origin = http://localhost:5000 remove both the port and : from the string
+        let url = document.location.origin.replace(/:\d+$/, '') + `:${i}/resources/data/server.json`;
 
-            let url = document.location.origin.replace(/:\d+$/, '') + `:${i}/resources/data/server.json`;
-            console.log(url);
-
-            res.open('GET', url, true);
-            res.send(
-                res.onreadystatechange = function () {
-                    if (res.readyState == 4) {
-                        if (res.status == 200) {
-                            console.log(`Port ${i} is open`)
-                        } else {
-                            console.log(`Port ${i} is closed`)
-                        }
-                    }
-                }
-            )
-            if (res.ok) {
-                console.log(`Port ${i} is open`)
-            }
-
-        } catch (error) {
-            console.log(error);
-        }
+        get_json(url);
 
     }
 }
 
 get_ports();
+
+async function get_json(url){
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
