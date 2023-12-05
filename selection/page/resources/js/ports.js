5e4db143c712cc55e1b3e9d24c43d06bf7e1fc23
Nigel Belderbos <nigel.belderbos@outlook.com>
2023-12-05 17:36:10 +0100

    all

---
+++ selection/page/resources/js/ports.js
@@ -0,0 +1,28 @@
+async function get_ports() {
+    for (let i = 5000; i < 5050; i++) {
+        try {
+            let res = new  XMLHttpRequest();
+            res.open('GET', `http://localhost:${i}`, true);
+            res.send(
+                res.onreadystatechange = function () {
+                    if (res.readyState == 4) {
+                        if (res.status == 200) {
+                            console.log(`Port ${i} is open`)
+                        } else {
+                            console.log(`Port ${i} is closed`)
+                        }
+                    }
+                }
+            )
+            if (res.ok) {
+                console.log(`Port ${i} is open`)
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+}
+
+get_ports();
