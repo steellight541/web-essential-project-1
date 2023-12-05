82557e12085b0d1b7ac02e148a92c86f62b86ccc
Nigel Belderbos <nigel.belderbos@outlook.com>
2023-12-05 17:54:41 +0100

    update to port searching

--- selection/page/resources/js/ports.js
+++ selection/page/resources/js/ports.js
@@ -1,10 +1,10 @@
 async function get_ports() {
     for (let i = 5000; i < 5010; i++) {
         try {
-            let res = new  XMLHttpRequest();
+            let res = new XMLHttpRequest();
             // document.location.origin = http://localhost:5000 remove both the port and : from the string
 
-            let url = document.location.origin.replace(/:\d+$/, '') + `:${i}/website/resources/data/server.json`;
+            let url = document.location.origin.replace(/:\d+$/, '') + `:${i}/resources/data/server.json`;
             console.log(url);
 
             res.open('GET', url, true);
