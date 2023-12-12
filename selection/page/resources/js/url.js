94d4524b9eb3fe15d2ba02ce1b0ba1bd3f85bf8b
Nigel Belderbos <nigel.belderbos@outlook.com>
2023-12-12 11:22:56 +0100

    transfer location

--- selection/page/resources/js/url.js
+++ selection/page/resources/js/url.js
@@ -1,3 +1,3 @@
 function browseTo(port) {
-    document.location.href = document.location.host + ":" + port;
+    document.location.href = document.location.host.slice(0, document.location.host.length - 5) + ":" + 5000;
 }
